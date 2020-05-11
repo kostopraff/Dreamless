09e80e55f26f60f102822130219b73bbfa2ffd79
Max <KamnevMD@list.ru>
2020-05-11 20:26:12 +0300

    Migrate to LoremPicsum api & clean code

--- app/src/main/java/ru/kostopraff/dreamless/PhotoDream.java
+++ app/src/main/java/ru/kostopraff/dreamless/PhotoDream.java
@@ -1,32 +1,23 @@
 package ru.kostopraff.dreamless;
 
+import android.graphics.Point;
 import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
 import android.os.Build;
+import android.os.Handler;
 import android.service.dreams.DreamService;
+import android.view.Display;
 import android.widget.ImageView;
-
 import androidx.annotation.RequiresApi;
-
-import com.google.gson.Gson;
 import com.squareup.picasso.Picasso;
-
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Scanner;
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
-
-import ru.kostopraff.dreamless.R;
-import ru.kostopraff.dreamless.Unsplash;
 
 public class PhotoDream extends DreamService {
 
     private ImageView imageView;
     private Drawable drawable;
+    private int width, height;
     @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     public void onAttachedToWindow() {
@@ -35,63 +26,36 @@ public class PhotoDream extends DreamService {
         setInteractive(false);
         setContentView(R.layout.dream_photo);
 
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        width = size.x;
+        height = size.y;
+
         imageView = findViewById(R.id.dream_photo);
         imageView.setImageResource(R.drawable.test_squirrel);
-        Timer timer = new Timer();
-        MyTask loadImage = new MyTask();
-        timer.schedule(loadImage, 0,30000);
     }
 
     @Override
     public void onDreamingStarted() {
         super.onDreamingStarted();
-
-    }
-
-    private class MyTask extends TimerTask{
-        @Override
-        public void run() {
-            Dream dream = new Dream();
-            dream.execute();
-        }
-    }
-
-    private class Dream extends AsyncTask<Void, Void, String>{
-        HttpURLConnection http;
-        @Override
-        protected String doInBackground(Void... voids) {
-            try {
-                URL url = new URL("https://api.unsplash.com/photos/random/?client_id=7AxaQaI1SBW6_mEmazBw3_EQvC6ljJLKRrBYphexCzc");
-                http = (HttpURLConnection) url.openConnection();
-                http.setRequestMethod("GET");
-                http.connect();
-
-                InputStream inputStream = http.getInputStream();
-                Scanner scanner = new Scanner(inputStream);
-                StringBuilder builder = new StringBuilder();
-                while (scanner.hasNextLine()) {
-                    builder.append(scanner.nextLine());
-                }
-                return builder.toString();
-            } catch (IOException e) {
-                //e.printStackTrace();
+        final Handler uiHandler = new Handler();
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                drawable = imageView.getDrawable();
+                uiHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Picasso.get()
+                                .load("https://picsum.photos/"+width+"/"+height+"?random="+new Random().nextInt())
+                                .placeholder(drawable)
+                                .priority(Picasso.Priority.HIGH)
+                                .into(imageView);
+                    }
+                });
             }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-
-            Gson g = new Gson();
-            Unsplash unsplash = g.fromJson(s, Unsplash.class);
-
-            drawable = imageView.getDrawable();
-            Picasso.get()
-                    .load(unsplash.getUrls().getRegular())
-                    .placeholder(drawable)
-                    .priority(Picasso.Priority.HIGH)
-                    .into(imageView);
-        }
+        },0,60000);
     }
 }
